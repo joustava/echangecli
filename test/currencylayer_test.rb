167b5ce00f3b8f66367d5d5d9218524a4259d8fa
joustava <joustava@gmail.com>
2018-06-27 10:29:37 +0200

    CurrencyLayer source other than USD wont work unless one pays

--- test/currencylayer_test.rb
+++ test/currencylayer_test.rb
@@ -4,7 +4,7 @@ class ExchangeCLITest < Minitest::Test
 
   def test_fetching_live_endpoint
     api = ::ExchangeCLI::CurrencyLayer.new
-    body = api.quotes('live')
+    body = api.quotes("live")
 
     assert body.keys == [
       :success,
@@ -18,7 +18,7 @@ class ExchangeCLITest < Minitest::Test
 
   def test_fetching_historical_endpoint
     api = ::ExchangeCLI::CurrencyLayer.new
-    body = api.quotes('historical', "2018-01-07")
+    body = api.quotes("historical", "usd", "2018-01-07")
 
     assert body.keys == [
       :success,
@@ -29,6 +29,17 @@ class ExchangeCLITest < Minitest::Test
       :timestamp,
       :source,
       :quotes
+    ]
+  end
+
+  # Will error unless you pay
+  def test_fetching_live_endpoint_non_usd
+    api = ::ExchangeCLI::CurrencyLayer.new
+    body = api.quotes('historical', "eur", "2018-01-07")
+
+    assert body.keys == [
+      :success,
+      :error
     ]
   end
 end
